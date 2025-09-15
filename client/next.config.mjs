18e10a9a9f319e9ad896dcee62c9e9745fd14637
Miau <165878715+bema96@users.noreply.github.com>
2025-09-15 16:31:48 +0200

    Fil struktur oprettet

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:4000/api/:path*',
+      },
+      {
+        source: '/images/:path*',
+        destination: 'http://localhost:4000/images/:path*',
+      },
+    ];
+  },
+};
 
 export default nextConfig;
